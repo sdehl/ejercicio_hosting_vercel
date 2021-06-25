ee07f2aa4381007e16eb50e525b89d54ab678b0c
sdehl <sjdehlr@gmail.com>
2021-06-24 21:23:02 -0300

    create vercel.js

---
+++ vercel.js
@@ -0,0 +1,15 @@
+{
+    "version": 2,
+    "builds": [
+      { "src": "index.js", "use": "@vercel/node" },
+      { "src": "/public/**/*", "use": "@vercel/static" }
+    ],
+  "routes": [ {
+        "src": "/css/(.*)",
+        "dest": "/public/css/$1"
+      },
+      {
+        "src": "/(.*)",
+        "dest": "/index.js"
+  } ]
+  }
