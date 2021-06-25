5ccceac7c92c94126ef85c18e995a69413b40db4
sdehl <sjdehlr@gmail.com>
2021-06-24 21:35:24 -0300

    versión 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.end("Llegaste a mi sitio versión 1");
+  res.end("Llegaste a mi sitio Versión 2");
 });
 
 app.get("/ejemplo", (req, res) => {
